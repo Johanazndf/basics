ad8308eeee2f04ebae1289e08e89b6abfa906891
Johanazndf <johanazndf@gmail.com>
2025-02-24 23:42:35 +0530

    Delete twodarray.java

--- twodarray.java
+++
@@ -1,7 +0,0 @@
-public class twodarray {
-    public static void main(String[] args) {
-        // 2D array = An array where each element is an array
-        // useful for storing a matrix of  data
-
-    }
-}
